a3ba721c3a424697d3cb4cdd31e2639ca1275c66
Mark <mark1mcerlean@gmail.com>
2022-03-15 10:24:30 +0000

    First Commit
    Start of assignment

---
+++ LibraryLoans/src/libraryloans/LibraryLoans.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package libraryloans;
+
+/**
+ *
+ * @author mark1
+ */
+public class LibraryLoans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
