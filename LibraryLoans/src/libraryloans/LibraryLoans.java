95070393f4920d3cc648a36e47c482e2c483431b
Mark <mark1mcerlean@gmail.com>
2022-03-16 16:57:07 +0000

    Commit 3
    -Added csv files to read from and write to.
    -program scans through and prints the contents of each file.
    ------------------------------
    - needs to add to lists instead of printing the contents of each file

--- LibraryLoans/src/libraryloans/LibraryLoans.java
+++ LibraryLoans/src/libraryloans/LibraryLoans.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package libraryloans;
-
+import java.util.Scanner;
+import java.io.*;
+import java.time.*;
 /**
  * Program is intended to store and manage details of 
  * library items currently on loan to library users according
@@ -25,11 +27,79 @@ public class LibraryLoans {
      *  as a starting point
      */
     
+    private String[] itemsList = {};
+    private String[] loansList = {};
+    private String[] usersList = {};
+           
+    
+    
+    private void readItems(){
+        try{            
+            File items = new File ("D:\\Java stuff\\LibraryLoans\\LibraryLoans\\LibraryLoans\\src\\libraryloans\\ITEMS.csv");
+        Scanner myReader = new Scanner(items);
+        myReader.useDelimiter(","); // sets a comma as the delimiter
+        
+        while (myReader.hasNextLine())
+        {
+            String data = myReader.nextLine();
+            System.out.println(data);
+        }
+            myReader.close();
+        } catch (FileNotFoundException e){
+            System.out.println("An error has occured.");
+            
+        }
+    }
  
+    private void readLoans(){
+        try{            
+            File loans = new File ("D:\\Java stuff\\LibraryLoans\\LibraryLoans\\LibraryLoans\\src\\libraryloans\\LOANS.csv");
+        Scanner myReader = new Scanner(loans);
+        myReader.useDelimiter(","); // sets a comma as the delimiter
+        
+        while (myReader.hasNextLine())
+        {
+            String data = myReader.nextLine();
+            System.out.println(data);
+        }
+            myReader.close();
+        } catch (FileNotFoundException e){
+            System.out.println("An error has occured.");
+            
+        }
+    }
+    
+     private void readUsers(){
+        try{            
+            File users = new File ("D:\\Java stuff\\LibraryLoans\\LibraryLoans\\LibraryLoans\\src\\libraryloans\\USERS.csv");
+        Scanner myReader = new Scanner(users);
+        myReader.useDelimiter(","); // sets a comma as the delimiter
+        
+        while (myReader.hasNextLine())
+        {
+            String data = myReader.nextLine();
+            System.out.println(data);
+        }
+            myReader.close();
+        } catch (FileNotFoundException e){
+            System.out.println("An error has occured.");
+            
+        }
+    }
+    
+    
+    public void start(){
+        this.readItems();
+        this.readLoans();
+        this.readUsers();
+    }
+    
     
     public static void main(String[] args) {
         // items, users and loans stored as lists/arrays
+        LibraryLoans libLoan = new LibraryLoans();
         
+        libLoan.start();
         
         // Wnhen program exits, list of loans should be written to LOANS.csv
     }
